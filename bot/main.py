c1682045d8c11810903b8f6b8d7f1f181e9cf3b5
Khaiam <khaiam@mail.ru>
2024-03-11 17:34:41 +0300

    load dotenv added

--- bot/main.py
+++ bot/main.py
@@ -1,13 +1,21 @@
 import asyncio
 import logging
+import os
+import sys
+sys.path.append(os.path.dirname(os.getcwd()))
 from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 from bot.config_reader import load_config
+from dotenv import load_dotenv
 
 from bot.handlers.default_commands import default_cmd_router
 from bot.handlers.admin_handlers import admin_router
 from bot.handlers.voice_handlers import voice_router
 from bot.handlers.worker_handlers import worker_router
+
+
+load_dotenv()  # take environment variables from .env.
+
 
 
 async def main():
